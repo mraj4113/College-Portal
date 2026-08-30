59c426dd65b442b2005e3c912d5742eab32901e5
mraj4113 <mayankr.ug24.ec@nitp.ac.in>
2026-08-30 18:06:05 +0530

    User.java

--- democollege/src/main/java/com/college/democollege/model/User.java
+++ democollege/src/main/java/com/college/democollege/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
